3d0f86c66fd23da07b16cd4fb3d6de767ad25e24
Simeon Wong <dtxe@users.noreply.github.com>
2025-03-24 15:20:01 -0400

    update assignment autograder: rename Q9 in output to Part 2 Q1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,7 +39,6 @@ rf -rf ./data
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
 
-
 ###########################################
 
 echo "Project setup is complete!"
